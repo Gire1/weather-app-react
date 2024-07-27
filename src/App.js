2b15b6dd68137e764177b41e5fdeedd91d92c929
Gire1 <168452060+Gire1@users.noreply.github.com>
2024-07-27 21:42:53 +0200

    Fixed warnings

--- src/App.js
+++ src/App.js
@@ -8,14 +8,13 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
-        <Weather defaultcity="Lisbon"/>
+      <Weather defaultcity="Lisbon"/>
         <footer>
           This project was coded by{""}
           <a href="https://github.com/Gire1/weather-app-react" target="_blank" rel="noreferrer">
             Grace Chifamba
           </a>{" "}
-          {""}and is{""}
+          and is{""}
           <a href="https://github.com/Gire1/weather-app-react" target="_blank" rel="noreferrer">
             open-sourced on GitHub{" "}
           </a>
